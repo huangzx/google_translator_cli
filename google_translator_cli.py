e68b959c6a5f270f8e686ffbd8d3cc250779da69
Zhongxin Huang <huangzhongxin@ivali.com>
2013-05-05 14:11:40 +0800

    add -s para for input sentence

--- google_translator_cli.py
+++ google_translator_cli.py
@@ -15,9 +15,9 @@ VERSION = '0.1'
 
 class HtmlParser(HTMLParser):
     ''' HTML 解析
-    
+
     找到 Google 翻译的结果
-    
+
     '''
     def handle_data(self, data):
         ''' 处理文本元素 '''
@@ -27,13 +27,14 @@ class HtmlParser(HTMLParser):
             if key == 'TRANSLATED_TEXT':
                 print value
                 break
+
     def close(self):
         HTMLParser.close(self)
 
 
 def translate(text, source, target):
     ''' Google 翻译
-    
+
     Args:
       text: 待翻译文本
       source: 源语言
@@ -46,7 +47,7 @@ def translate(text, source, target):
     req = urllib2.Request(url, data)
     req.add_header('User-Agent', 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727)')
     response = urllib2.urlopen(req)
-    htmlparser= HtmlParser()
+    htmlparser = HtmlParser()
     htmlparser.feed(response.read())
     htmlparser.close()
 
@@ -55,20 +56,22 @@ def main():
     argvs = sys.argv[1:]
     if not argvs:
         argvs = ['-h']
-    
-    desc = 'Google translator cli, defaults to en -> zh-CN'
+
+    desc = 'Google translator cli, en -> zh-CN by default.'
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('-v', '--version', action='store_true',
                         dest='v', help='show version')
+    parser.add_argument('-s', '--sentence', action='store_true',
+                        dest='s', help='strings regarded as a sentence')
     parser.add_argument('-r', '--reverse', action='store_true',
-                        dest='r', help='reverse default langpair, a.k.s zh-CN -> en')
+                        dest='r', help='reverse default langpair as zh-CN -> en')
     parser.add_argument('-f', '--from', nargs=1, metavar='string',
                         default='en', dest='f', help='source language')
     parser.add_argument('-t', '--to', nargs=1, metavar='string',
                         default='zh-CN', dest='t', help='target language')
-    parser.add_argument('string', nargs='*', help='given string')
+    parser.add_argument('string', nargs='*', help='strings')
     args = parser.parse_args(argvs)
-    
+
     if args.v:
         print(VERSION)
         sys.exit()
@@ -76,8 +79,12 @@ def main():
     if args.r:
         args.f = ['zh-CN']
         args.t = ['en']
-    
-    for x in args.string:
+
+    strings = args.string
+    if args.s:
+        strings = [' '.join(args.string)]
+
+    for x in strings:
         translate(x, args.f[0], args.t[0])
 
 
